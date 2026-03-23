1a19ee3a4c0b3227b1c09e17e197b5d7f7b37c4f
Sahil <your-sahilmeshram1917@gm.com>
2026-03-24 02:53:23 +0530

    feat: added custom app icon

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kilogram.user',
+  appName: 'Kilogram ',
+  webDir: 'dist'
+};
+
+export default config;
